773fb4e30e9aa06d167e1f3f4b57ce597dd55983
guohongwei719 <guohongwei04441@hellobike.com>
2019-08-30 14:23:08 +0800

    菜单处理

--- GHWExtension/SourceEditorExtension.m
+++ GHWExtension/SourceEditorExtension.m
@@ -17,12 +17,25 @@
 }
 */
 
-/*
+
 - (NSArray <NSDictionary <XCSourceEditorCommandDefinitionKey, id> *> *)commandDefinitions
 {
     // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
-    return @[];
+
+    
+    return @[@{XCSourceEditorCommandClassNameKey: @"SourceEditorCommand",
+               XCSourceEditorCommandIdentifierKey: @"com.jingyao.GHWXcodeExtension.GHWExtension.sortImport",
+               XCSourceEditorCommandNameKey: @"sortImport"
+               },
+             @{XCSourceEditorCommandClassNameKey: @"SourceEditorCommand",
+               XCSourceEditorCommandIdentifierKey: @"com.jingyao.GHWXcodeExtension.GHWExtension.initView",
+               XCSourceEditorCommandNameKey: @"initView"
+               },
+             @{XCSourceEditorCommandClassNameKey: @"SourceEditorCommand",
+               XCSourceEditorCommandIdentifierKey: @"com.jingyao.GHWXcodeExtension.GHWExtension.addLazyCode",
+               XCSourceEditorCommandNameKey: @"addLazyCode"
+               }];
 }
-*/
+
 
 @end
